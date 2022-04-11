f045b23def706179ad30816e0cd0467fb3f1e2bc
J S Packiaraj <jsp@jsp.net.in>
2022-04-11 18:13:12 +0530

    Class at work

---
+++ 20220411/untitled/src/TestBookWithHelperFunction.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class TestBookWithHelperFunction {
+    static String title, author, isbn;
+    public static void main(String[] args) {
+        {
+            // String title, author, isbn;
+            Book FirstBook = new Book();
+            System.out.println("Result of the first Constructor:");
+            System.out.println("The default book is " + FirstBook.getTitle()
+                    + " by " + FirstBook.getAuthor()
+                    + " with ISBN number " + FirstBook.getIsbn());
+
+            getBookInfo();
+            Book SecondBook = new Book(title, author, isbn);
+            System.out.println("The entered book is " + SecondBook.getTitle()
+                    + " by " + SecondBook.getAuthor()
+                    + " with ISBN number " + SecondBook.getIsbn());
+        }
+    }
+    static void getBookInfo(){
+        Scanner inputObject = new Scanner(System.in);
+        System.out.print("Enter the Title of the Book : ");
+        title = inputObject.nextLine();
+        System.out.print("Enter the author of the Book : ");
+        author = inputObject.nextLine();
+        System.out.print("Enter the ISBN number of the Book : ");
+        isbn = inputObject.nextLine();
+    }
+}
